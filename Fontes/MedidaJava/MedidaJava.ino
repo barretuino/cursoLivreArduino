82ad1a8007e569d029455a2a40346d6ef7fff088
Barretuino Tecnologia <41486280+barretuino@users.noreply.github.com>
2019-09-22 14:04:09 -0300

    Add files via upload

---
+++ Fontes/MedidaJava/MedidaJava.ino
@@ -0,0 +1,20 @@
+/**
+ * Aquisição de Medidas via Portas Análogicas
+ * Prof. Paulo Barreto
+ * Data 21/09/2019
+ */
+#define SENSOR A0
+ 
+void setup(){
+  Serial.begin(9600);  
+  pinMode(SENSOR, INPUT);
+}
+
+void loop(){
+  int medida = analogRead(SENSOR);
+  
+  Serial.print("$");
+  Serial.print(medida);
+  Serial.println("#");
+  delay(1000);
+}
